a7cc14208ae3ba9803b834244acf3ee64e37139c
mcle0341 <mcle0341@algonquinlive.com>
2016-03-15 18:18:48 -0400

    added css styles

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
-grains.forEach(function (item) {
-  var $ul = $('<ul>');
- 
-  $ul.append('li');
-  
+
+
+grains.forEach(function (grain) {
+  var $ul = $('ul');
+  var $li = $('<li>');
+  var $name = $('<h2>');
+  var $img = $('<img>');
+  var $p = $('<p>');
+
+  $name.html(grain.name);
+  $img.attr('src', 'images/' + grain.img);
+  $p.html(grain.desc);
+  $li.append($img, $name, $p);
+  $ul.append($li);
 });
